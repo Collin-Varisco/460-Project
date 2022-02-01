ac816f6808aa599ea053bddf2735c5944077e0f8
Crisco <crisco@Collins-MacBook-Air.local>
2022-02-01 12:47:28 -0600

    First Commit for project.

---
+++ main.py
@@ -0,0 +1,106 @@
+import sqlite3
+
+""" Try to establish a connection with the database """
+def create_connection(db):
+    con = None
+    try:
+        con = sqlite3.connect(db)
+        return con
+    except ValueError as e:
+        print(e)
+    return con
+
+connection = create_connection("py.db")
+
+""" create a table from the createSqlTable statement
+:param conn: Connection object
+:param createSqlTable: a CREATE TABLE statement
+:return:
+"""
+def createTable(conn, createSqlTable):
+    try:
+        c = conn.cursor()
+        c.execute(createSqlTable)
+    except ValueError as e:
+        print(e)
+
+def insertEmployee(conn):
+    try: 
+        print("--- Insert Employee Information ---")
+        eid = input("Employee ID: ")
+        fname = input("First Name: ")
+        lname = input("Last Name: ")
+        pos = input("Position: ")
+        fid = input("Franchise ID: ")
+        employeeSql = """INSERT INTO Employee
+                         (EmpID, Fname, Lname, Position, FID )
+                         VALUES (?, ?, ?, ?, ?);"""
+        tuple = (eid, fname, lname, pos, fid)
+        c = conn.cursor()
+        c.execute(employeeSql, tuple)
+        conn.commit()
+        c.close()
+    except sqlite.Error as error:
+        print("//")
+        print(error)
+    finally:
+        if conn:
+            conn.close()
+            print("Connection is closed.")
+                                            
+
+def createTableMenu(conn):
+    print("--- Options ---")
+    print("1. Add an Employee")
+    try: 
+        m_option = int(input(" >> "))
+        if(m_option == 1):
+            repeat = True
+            while(repeat):
+                insertEmployee(conn);
+                optionRepeat = input("Add another employee? (Y/N): ")
+                if(optionRepeat == "N"):
+                    repeat = False
+        else:
+            print("--------")
+    except ValueError as e:
+        print(e)
+        
+
+def main():
+    database = "MovingCompany.db"
+    
+    create_franchise_table = """CREATE TABLE IF NOT EXISTS Franchise (
+                                       FID               integer     PRIMARY KEY,
+                                       StreetAddress     text        NOT NULL,
+                                       City              text        NOT NULL,
+                                       State             text        NOT NULL,
+                                       ZipCode           integer     NOT NULL,
+                                       OwnerFname        text        NOT NULL,
+                                       OwnerLname        text        NOT NULL
+                                );"""
+
+    create_employee_table = """CREATE TABLE IF NOT EXISTS Employee (
+                                   EmpID     integer     PRIMARY KEY,
+                                   Fname     text        NOT NULL,
+                                   Lname     text        NOT NULL,
+                                   Position  text        NOT NULL,
+                                   FID       integer     NOT NULL,
+                                   FOREIGN KEY (FID) REFERENCES Franchise (FID)
+                            );"""
+
+    conn = create_connection(database)
+
+    if conn is not None:
+        createTable(conn, create_franchise_table)
+        createTable(conn, create_employee_table);
+        #createTableMenu(conn)
+        c = conn.cursor();
+        check = c.execute("SELECT * FROM Employee").fetchall()
+        print(check);
+    #else:
+    #    print("ERROR: Could not establish database connection.")
+
+if __name__ == '__main__':
+    main()
+
